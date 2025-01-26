078d2af29e0d442daa2fa54cb40ec733e598e35e
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-01-26 21:45:07 +0530

    Add files via upload

---
+++ Day1/01-get-started.js
@@ -0,0 +1,9 @@
+// single line comment
+
+/* 
+This is multiple line comment - Shift + Alt + A
+*/
+
+// To print a statment in console
+
+console.log("Welcome to Testleaf");
---
+++ Day1/04-hoisting.js
@@ -0,0 +1,14 @@
+
+
+/* console.log(company) 
+var company = "testleaf"// declaration and initialization
+// testleaf */
+
+console.log(company) 
+let companyName = "testleaf"// declaration and initialization
+console.log(company)// testleaf
+
+console.log(company) 
+const company = "testleaf"// declaration and initialization
+
+// testleaf
